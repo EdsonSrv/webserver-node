fe24e4624d0f40a7050697d9a4f00d54f52876a8
Edson Srvn <edson@ciencias.unam.mx>
2021-10-27 17:27:43 -0500

    Using static content

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
- 
-app.get('/', (req, res) => {
-  res.send('Home Page')
-})
+
+//Servir contenido estático
+app.use( express.static('public') )
 
 app.get('/hola-mundo', (req, res) => {
   res.send('Hola mundo en su repectiva ruta')
 })
 
 app.get('*', (req, res) => {
-  res.send('404 | Page not found')
+  res.sendFile(__dirname + '/public/404.html')
 })
  
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
