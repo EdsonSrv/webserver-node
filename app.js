a61b5e85567b16c3087ab9b974c13c544c665655
Edson Srvn <edson@ciencias.unam.mx>
2021-10-29 13:28:17 -0500

    AModify the link url

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const port = 3000
 //Servir contenido estático
 app.use( express.static('public') )
 
-app.get('/hola-mundo', (req, res) => {
-  res.send('Hola mundo en su repectiva ruta')
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html')
+})
+
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/public/elements.html')
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/404.html')
+  res.sendFile(__dirname + '/public/back/404.html')
 })
  
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
